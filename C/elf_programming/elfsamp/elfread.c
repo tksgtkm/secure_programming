861e1ac76b06193e2a6ea649b5b8bac05dc48a93
tksgtkm <h9cd342fd7jgx36@gmail.com>
2025-09-02 00:05:42 +0900

    elf 解析プログラムの作成

---
+++ C/elf_programming/elfsamp/elfread.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <elf.h>
+
+int main() {
+    Elf64_Ehdr *ehdr;
+
+    ehdr = (Elf64_Ehdr *)0x0000000000000000;
+
+    printf("0x%02x%c%c%c\n",
+        ehdr->e_ident[EI_MAG0],
+        ehdr->e_ident[EI_MAG1],
+        ehdr->e_ident[EI_MAG2],
+        ehdr->e_ident[EI_MAG3]
+    );
+
+    exit(0);
+}
