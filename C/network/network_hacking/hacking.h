31739a293639c4213ee9607b25ef05faee683162
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-01-18 00:48:28 +0900

    web server implemented

---
+++ C/network/network_hacking/hacking.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void fatal(char *message) {
+    char error_message[100];
+
+    strcpy(error_message, "[!!] fatal error : ");
+    strncat(error_message, message, 83);
+    perror(error_message);
+    exit(-1);
+}
+
+void *ec_malloc(unsigned int size) {
+    void *ptr;
+    ptr = malloc(size);
+    if (ptr == NULL) 
+        fatal("faild to allocation memory in ec_malloc()");
+    return ptr;
+}
+
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+    unsigned char byte;
+    unsigned int i, j;
+    for (i = 0; i < length; i++) {
+        byte = data_buffer[i];
+        printf("%02x ", data_buffer[i]);
+        if (((i % 16) == 15) || (i == length - 1)) {
+            for (j = 0; j < 15 - (i % 16); j++)
+                printf("    ");
+            printf("| ");
+            for (j = (i - (i % 16)); j <= i; j++) {
+                byte = data_buffer[j];
+                if ((byte > 31) && (byte < 127))
+                    printf("%c", byte);
+                else
+                    printf(".");
+            }
+            printf("\n");
+        }
+    }
+}
--- C/network/packet_demo/utils.cc
+++ C/network/packet_demo/utils.cc
@@ -12,3 +12,35 @@ uint32_t swap_byte_order_32(uint32_t v) {
            (v & 0x00ff0000) >> 8 |
            (v & 0xff000000) >> 24;
 }
+
+uint16_t ntohs(uint16_t v) {
+    return swap_byte_order_16(v);
+}
+
+uint16_t htons(uint16_t v) {
+    return swap_byte_order_16(v);
+}
+
+uint32_t ntohl(uint32_t v) {
+    return swap_byte_order_32(v);
+}
+
+uint32_t htonl(uint32_t v) {
+    return swap_byte_order_32(v);
+}
+
+uint8_t ip_string_pool_index = 0;
+char ip_string_pool[4][16];
+
+const char *ip_ntoa(uint32_t in) {
+    uint8_t a = in & 0x000000ff;
+    uint8_t b = in >> 8 & 0x000000ff;
+    uint8_t c = in >> 16 & 0x000000ff;
+    uint8_t d = in >> 24 & 0x000000ff;
+    ip_string_pool_index++;
+    ip_string_pool_index %= 4;
+    sprintf(ip_string_pool[ip_string_pool_index],
+            "%d.%d.%d.%d", a, b, c, d);
+    return ip_string_pool[ip_string_pool_index];
+}
+
