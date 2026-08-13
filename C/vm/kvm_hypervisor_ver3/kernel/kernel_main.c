0dd8903ba432046e83f7678b4dcfa98414646c3d
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-08-14 01:27:33 +0900

    utils実装

--- C/vm/kvm_hypervisor_ver3/kernel/kernel_main.c
+++ C/vm/kvm_hypervisor_ver3/kernel/kernel_main.c
@@ -3,3 +3,25 @@
 #define MSR_CSTAR 0xc0000083
 #define MSR_SYSCALL_MASK 0xc0000084
 
+int register_syscall() {
+    asm(
+        "xor rax, rax;"
+        "mov rdx, 0x00200008;"
+        "mov ecx, %[msr_star];"
+        "wrmsr;"
+
+        "mov eax, %[fmask];"
+        "xor rdx, rdx;"
+        "mov ecx, %[msr_fmask];"
+        "wrmsr;"
+
+        "lea rax, [rip + syscall_entry];"
+        "mov rdx, %[base] >> 32;"
+        "mov ecx, %[msr_syscall];"
+        "wrmsr;"
+        :: [msr_star]"i"(MSR_STAR),
+           [fmask]"i"(0x3f7fd5), [msr_fmask]"i"(MSR_SYSCALL_MASK),
+           [base]"i"
+    );
+    return 0;
+}
